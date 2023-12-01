ab9c034ab6d6a578fa0e574ef8c1b8d117b8bfc9
anquii <50277099+anquii@users.noreply.github.com>
2023-12-01 19:45:57 +0000

    Updated FixedWidthInteger.swift

--- Sources/BinaryExtensions/FixedWidthInteger.swift
+++ Sources/BinaryExtensions/FixedWidthInteger.swift
@@ -2,13 +2,13 @@ import Foundation
 
 public extension FixedWidthInteger {
     init?(data: Data) {
-        let bytes = data.withUnsafeBytes {
-            $0.load(as: Self.self)
+        guard let value = Self(data.hexEncodedString(stripLeadingZeroes: false), radix: 16) else {
+            return nil
         }
-        self = bytes.byteSwapped
+        self = value
     }
 
     var bytes: [UInt8] {
-        withUnsafeBytes(of: byteSwapped, Array.init)
+        withUnsafeBytes(of: self, Array.init)
     }
 }
